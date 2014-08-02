037cbd993f407ffe5aea773d2156421f7a696fa4
Doug Modie <dmodie@gmail.com>
2014-08-02 15:20:30 -0700

    could turn in now

--- Utilities.js
+++ Utilities.js
@@ -2,7 +2,6 @@
 var Utilities = (function() {
     var indents = 0;
     return {
-
         /**
          * Number validator for an input text field.  Apply the following to the input field:
          *          onkeypress="Utilities.numericValidation(event)"
@@ -25,7 +24,7 @@ var Utilities = (function() {
 
         /**
          /**
-         * For internal use only this is used to stop the input event.  Use if validation failed.
+         * Stop the input event.  Use if validation failed.
          * @private
          * @param event
          */
@@ -37,7 +36,6 @@ var Utilities = (function() {
                 event.returnValue = false; // older deprecated event cancelation
             }
         },
-
 
         /**
          * Checks if field is empty on blur.  If so, put the default value, such as 'Enter Age' back in.
@@ -67,7 +65,7 @@ var Utilities = (function() {
         },
 
         /**
-         * Takes in raw numbers and converts to a currency format for HTML display.  Adds symbol, commas, and rounds
+         * Takes in raw number and converts to a currency format for HTML display.  Adds symbol, commas, and rounds
          * to two decimal places
          *
          * @param type - currently support Utilities.EURO, Utilities.DOLLAR, Utilities.POUND;
@@ -100,96 +98,6 @@ var Utilities = (function() {
             }
         },
 
-
-        /**
-         * Traverses the array recursively, prints to log when it gets to a key or a leaf, otherwise, recurses.
-         *
-         * @param o - the object
-         * @private
-         */
-        inspectArray : function inspectArray(array){
-            var spaces='';
-            for (var i = 0;i<indents;i++){
-                spaces = spaces.concat("    ");
-            }
-            var str = spaces.concat ("[");
-            console.log(str)
-         //   console.log("[")
-            for (var i = 0;i< array.length; i++){
-                var type = typeof array[i];
-
-                if (Array.isArray(array[i])){
-                 //   indents++;
-                    Utilities.inspectArray(array[i]);
-
-                }
-
-
-
-
-                else if(typeof array[i] == "object"){
-                    Utilities.inspectObject(array[i]);
-                }
-              //  else if (){//check for array in this array
-                //
-               // }
-                else {
-                    str = spaces.concat (array[i]);
-                    console.log(str);
-                }
-
-            }
-            str = spaces.concat ("]");
-            console.log(str);
-
-        },
-
-
-        /**
-         * Logs a JSON type object to the console in so that the developer can quickly see the schema.
-         * Resets indentation, then calls _inspectObject
-         * @param o - the JSON object
-         */
-        inspect: function inspect(o){
-            indents=0;
-            Utilities.inspectObject(o);
-
-        },
-        /**
-         * Traverses the object recursively, prints to log when it gets to a key or a leaf, otherwise, recurses.
-         *
-         * @param o - the object
-         * @private
-         */
-        inspectObject : function inspectObject(o){
-            for (var key in o){
-                var str = "";
-                var spaces = "";
-                for (var i = 0;i<indents;i++){
-                    spaces = spaces.concat("    ");
-                }
-                str = spaces.concat ("{key: " + key);
-                console.log(str );
-                if (Array.isArray(o[key])){
-                    indents++;
-                    Utilities.inspectArray(o[key]);
-
-                }
-                else if (typeof o[key] == "object"){
-                    indents++;
-                    Utilities.inspectObject(o[key]);
-                }
-                else {
-                    str = spaces.concat (" val: " + o[key]);
-                    console.log(str );
-                  //  console.log("value: " + o[key]);
-                }
-                str = spaces.concat ("}" );
-                console.log(str)
-
-            }
-        },
-
         /**
          * Checks if string can be converted to number.
          *
@@ -215,6 +123,79 @@ var Utilities = (function() {
                 return parseFloat(str);
             }else {//otherwise return string
                 return str;
+            }
+        },
+
+        /**
+         * Logs a JSON type object to the console in so that the developer can quickly see the schema.
+         * Resets indentation, then calls _inspectObject
+         * @param o - the JSON object
+         */
+        inspect: function inspect(o){
+            indents=0;
+            Utilities._inspectObject(o);
+        },
+
+        /**
+         * Traverses the array recursively, prints to log when it gets to a key or a leaf, otherwise, recurses.
+         *
+         * @param o - the object
+         * @private
+         */
+        _inspectArray : function _inspectArray(array){
+            var spaces='';
+            for (var i = 0;i<indents;i++){
+                spaces = spaces.concat("    ");
+            }
+            var str = spaces.concat ("[");
+            console.log(str)
+            for (var i = 0;i< array.length; i++){
+                if (Array.isArray(array[i])){
+                    Utilities._inspectArray(array[i]);
+                }
+                else if(typeof array[i] == "object"){
+                    Utilities._inspectObject(array[i]);
+                }
+                else {
+                    str = spaces.concat (array[i]);
+                    console.log(str);
+                }
+            }
+            str = spaces.concat ("]");
+            console.log(str);
+        },
+
+        /**
+         * Traverses the object recursively, prints to log when it gets to a key or a leaf, otherwise, recurses.
+         *
+         * @param o - the object
+         * @private
+         */
+        _inspectObject : function _inspectObject(o){
+            for (var key in o){
+                var str = "";
+                var spaces = "";
+                for (var i = 0;i<indents;i++){
+                    spaces = spaces.concat("    ");
+                }
+                str = spaces.concat ("{key: " + key);
+                console.log(str );
+                if (Array.isArray(o[key])){
+                    indents++;
+                    Utilities._inspectArray(o[key]);
+
+                }
+                else if (typeof o[key] == "object"){
+                    indents++;
+                    Utilities._inspectObject(o[key]);
+                }
+                else {
+                    str = spaces.concat (" val: " + o[key]);
+                    console.log(str );
+                  //  console.log("value: " + o[key]);
+                }
+                str = spaces.concat ("}" );
+                console.log(str)
             }
         }
     };
